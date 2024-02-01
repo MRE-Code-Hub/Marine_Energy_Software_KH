b93236b323c3314e0aa97d3f312343cbd3099f95
Mathew Topper <damm_horse@yahoo.co.uk>
2024-02-01 16:53:28 +0000

    Add automated generation of glossary file

--- format.py
+++ format.py
@@ -67,6 +67,7 @@ def main():
     
     setup_format(subparsers)
     setup_template(subparsers)
+    setup_glossary(subparsers)
     
     args = parser.parse_args()
     args.func(args)
@@ -87,6 +88,15 @@ def setup_template(subparsers):
     parser.add_argument('--dir', default=".")
     parser.add_argument('--name', default="template.yaml")
     parser.set_defaults(func=lambda args: make_template(args.dir, args.name))
+
+
+def setup_glossary(subparsers):
+    parser = subparsers.add_parser(
+        'glossary',
+        help='create glossary')
+    parser.add_argument('--dir', default=".")
+    parser.add_argument('--name', default="dictionary.json")
+    parser.set_defaults(func=lambda args: make_glossary(args.dir, args.name))
 
 
 def formatter(path: Optional[StrOrPath]):
@@ -170,31 +180,31 @@ def get_template(json_schema: str) -> tuple[dict[str, Any],
     descriptions = {}
     
     for k, v in schema_deref['properties'].items():
-        process_schema_prop(template, k, v)
+        _add_prop_to_template(template, k, v)
         if "description" in v:
             descriptions[k] = v["description"]
     
     return template, descriptions
 
 
-def process_schema_prop(capture, k, v):
+def _add_prop_to_template(capture, k, v):
     
     if 'oneOf' in v:
-        process_schema_multi(capture, k, v, 'oneOf')
+        _add_multi_to_template(capture, k, v, 'oneOf')
     elif 'anyOf' in v:
-        process_schema_multi(capture, k, v, 'anyOf')
+        _add_multi_to_template(capture, k, v, 'anyOf')
     elif 'array' in v['type']:
         if 'enum' in v['items']:
             capture[k] = v['items']['enum']
         else:
             raise RuntimeError(f"Unrecognised items in {(v['items'])} for {k}")
     elif v['type'] == 'string':
-        process_schema_string(capture, k, v)
+        _add_string_to_template(capture, k, v)
     else:
         raise RuntimeError(f"Unrecognised type ({(v['type'])}) for {k}")
 
 
-def process_schema_string(capture, k, v):
+def _add_string_to_template(capture, k, v):
     
     if 'format' in v:
         if v['format'] == 'uri':
@@ -210,7 +220,7 @@ def process_schema_string(capture, k, v):
         capture[k] = SHORT_TEXT
 
 
-def process_schema_multi(capture, k, v, pk):
+def _add_multi_to_template(capture, k, v, pk):
     
     vpk = v[pk]
     types = {d['type']: i for i, d in enumerate(vpk)}
@@ -225,7 +235,7 @@ def process_schema_multi(capture, k, v, pk):
                 if 'properties' in subsub:
                     for sk, sv in subsub['properties'].items():
                         sd = {}
-                        process_schema_prop(sd, sk, sv)
+                        _add_prop_to_template(sd, sk, sv)
                         if sk in capture[k]:
                             capture[k].remove(sk)
                         capture[k].append(sd)
@@ -238,10 +248,148 @@ def process_schema_multi(capture, k, v, pk):
             raise RuntimeError(msg)
     elif 'string' in types:
         sub = vpk[types['string']]
-        process_schema_string(capture, k, sub)
+        _add_string_to_template(capture, k, sub)
     else:
         msg = f"Unrecognised types ({types}) for {k}"
         raise RuntimeError(msg)
+
+
+def make_glossary(template_dir: StrOrPath = ".",
+                  glossary_name: str = "dictionary.json"):
+    
+    db_repo_path = Path(".")
+    schema_path = db_repo_path / "schema.yaml"
+    assert schema_path.is_file()
+    
+    with open(schema_path, 'r') as f:
+        schema = load(f, Loader=Loader)
+    
+    json_schema = json.dumps(schema)
+    glossary = get_glossary(json_schema)
+    
+    template_path = Path(template_dir) / glossary_name
+    
+    with open(template_path, 'w') as f:
+        json.dump(glossary, f, indent=4)
+
+
+def get_glossary(json_schema: str) -> list[dict[str, Any]]:
+    
+    schema_deref = jsonref.loads(json_schema)
+    glossary = []
+    
+    for k, v in schema_deref['properties'].items():
+        _add_prop_to_glossary(glossary, k, v)
+    
+    return glossary
+
+
+def _add_prop_to_glossary(capture, k, v):
+
+    if 'oneOf' in v:
+        _add_multi_to_glossary(capture, k, v, 'oneOf')
+    elif 'anyOf' in v:
+        _add_multi_to_glossary(capture, k, v, 'anyOf')
+    elif 'array' in v['type']:
+        if 'enum' in v['items']:
+            record = {"title": k,
+                      "description": v["description"]}
+            terms = []
+            for value in v['items']['enum']:
+                term = {
+                    "name": value,
+                }
+                if ('meta:enum' in v['items'] and
+                    value in v['items']['meta:enum']):
+                    term["description"] = v['items']['meta:enum'][value]
+                terms.append(term)
+            record["terms"] =  terms
+            capture.append(record)
+        else:
+            raise RuntimeError(f"Unrecognised items in {(v['items'])} for {k}")
+    elif v['type'] == 'string':
+        _add_string_to_glossary(capture, k, v)
+    else:
+        raise RuntimeError(f"Unrecognised type: in {(v['type'])} for {k}")
+
+
+def _add_string_to_glossary(capture, k, v):
+    
+    record = {"title": k,
+              "description": v["description"]}
+    
+    if 'enum' in v:
+        terms = []
+        for value in v['enum']:
+            term = {
+                    "name": value,
+            }
+            if 'meta:enum' in v and value in v['meta:enum']:
+                term["description"] = v['meta:enum'][value]
+            terms.append(term)
+        record["terms"] =  terms
+
+    capture.append(record)
+
+
+def _add_multi_to_glossary(capture, k, v, pk):
+
+    vpk = v[pk]
+    types = {d['type']: i for i, d in enumerate(vpk)}
+
+    if 'array' in types:
+        sub = vpk[types['array']]
+        if 'anyOf' in sub['items']:
+            record = {"title": k,
+                      "description": v["description"]}
+            terms = []
+            term_indices = {}
+            term_counter = 0
+            for subsub in sub['items']['anyOf']:
+                if 'enum' in subsub:
+                    sub_terms = []
+                    for value in subsub['enum']:
+                        term = {
+                            "name": value,
+                        }
+                        if ('meta:enum' in subsub and
+                            value in subsub['meta:enum']):
+                            term["description"] = subsub['meta:enum'][value]
+                        sub_terms.append(term)
+                        term_indices[value] = term_counter
+                        term_counter += 1
+                    terms.extend(sub_terms)
+                if 'properties' in subsub:
+                    for sk, sv in subsub['properties'].items():
+                        sd = []
+                        _add_prop_to_glossary(sd, sk, sv)
+                        terms[term_indices[sk]]["terms"] = sd[0]["terms"]
+            record["terms"] =  terms
+            capture.append(record)
+        elif 'enum' in sub['items']:
+            record = {"title": k,
+                      "description": v["description"]}
+            terms = []
+            for value in sub['items']['enum']:
+                term = {
+                    "name": value,
+                }
+                if ('meta:enum' in sub['items'] and
+                    value in sub['items']['meta:enum']):
+                    term["description"] = sub['items']['meta:enum'][value]
+                terms.append(term)
+            record["terms"] =  terms
+            capture.append(record)
+        elif 'type' in sub['items'] and sub['items']['type'] == 'string':
+            record = {"title": k,
+                      "description": v["description"]}
+            capture.append(record)
+        else:
+            raise RuntimeError(f"Unrecognised items in {(sub['items'])} for {k}")
+    elif 'string' in types:
+        sub = vpk[types['string']]
+        sub["description"] = v["description"]
+        _add_string_to_glossary(capture, k, sub)
 
 
 if __name__ == "__main__":
